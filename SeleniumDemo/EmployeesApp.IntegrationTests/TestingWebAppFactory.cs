f38b72ad2bec6afcf33966099c9f12de5235b45f
tenevincent <vincent.tene@gmail.com>
2021-03-24 23:32:08 +0100

    new items added

---
+++ SeleniumDemo/EmployeesApp.IntegrationTests/TestingWebAppFactory.cs
@@ -0,0 +1,62 @@
+﻿using EmployeesApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmployeesApp.IntegrationTests
+{
+    public class TestingWebAppFactory<T> : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<EmployeeContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+                services.AddDbContext<EmployeeContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryEmployeeTest");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                services.AddAntiforgery(t =>
+                {
+                    t.Cookie.Name = AntiForgeryTokenExtractor.AntiForgeryCookieName;
+                    t.FormFieldName = AntiForgeryTokenExtractor.AntiForgeryFieldName;
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    using (var appContext = scope.ServiceProvider.GetRequiredService<EmployeeContext>())
+                    {
+                        try
+                        {
+                            appContext.Database.EnsureCreated();
+                        }
+                        catch (Exception ex)
+                        { //Log errors or do anything you think it's needed throw;
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
